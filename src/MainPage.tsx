c184d9b03c00cb1534d8d680a4c34b254d7c0063
brooks42 <britainsux@gmail.com>
2021-07-19 22:34:51 -0700

    did some stuff to limit memory usage of node when starting up so it can fit in heroku

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,29 +5,19 @@ import {
     TabPanels,
     TabList
 } from '@chakra-ui/react'
-import { CardSearchPage } from './CardSearchPage'
 import * as React from 'react'
-import { ComparisonView } from './data_views/ComparisonView'
 import { SetDatasView } from './SetDataView'
 
 
 export function MainPage() {
     return (<Tabs>
         <TabList>
-            <Tab>Search</Tab>
             <Tab>Some Data</Tab>
-            <Tab>Comparison</Tab>
         </TabList>
 
         <TabPanels>
             <TabPanel>
-                <CardSearchPage />
-            </TabPanel>
-            <TabPanel>
                 <SetDatasView />
-            </TabPanel>
-            <TabPanel>
-                <p><ComparisonView /></p>
             </TabPanel>
         </TabPanels>
     </Tabs>)
